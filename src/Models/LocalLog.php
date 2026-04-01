9e1d26264823e03c21757271dd24890de6e061f7
Roberto <rsimonru@gmail.com>
2026-04-01 11:43:39 +0200

    doLog method

--- src/Models/LocalLog.php
+++ src/Models/LocalLog.php
@@ -3,6 +3,7 @@
 namespace Devlab\LaravelLogs\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class LocalLog extends Model
 {
@@ -13,4 +14,26 @@ class LocalLog extends Model
     protected $table = 'logs';
     protected $fillable = ['procedure', 'created_user'];
 
+    public static function doLog($procedure, $data)
+    {
+        $user_id = Auth::user()->id ?? config('constants.users.system');
+        $procedure_id = self::checkProcedure($procedure);
+        $log = new self();
+        $log->procedure_id = $procedure_id;
+        $log->procedure = $procedure;
+        $log->data = $data;
+        $log->created_user = $user_id;
+        $log->save();
+    }
+
+    private static function checkProcedure($procedure)
+    {
+        $bd_procedure = ModelsProcedure::where('procedure', $procedure)->get()->first();
+        if (empty($bd_procedure)) {
+            $bd_procedure = new ModelsProcedure();
+            $bd_procedure->procedure = $procedure;
+            $bd_procedure->save();
+        }
+        return $bd_procedure->id;
+    }
 }
